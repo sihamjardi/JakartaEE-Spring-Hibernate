2c6b3344cca051d6cd62a829feb5e8b04fe4115e
Your Name <sihamjardi2@gmail.com>
2025-11-06 23:07:15 +0100

    first commit

--- src/main/java/com/example/entities/Machine.java
+++ src/main/java/com/example/entities/Machine.java
@@ -1,6 +1,6 @@
 package com.example.entities;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 import java.util.Date;
 
 @Entity
--- src/main/java/com/example/entities/Salle.java
+++ src/main/java/com/example/entities/Salle.java
@@ -1,7 +1,7 @@
 package com.example.entities;
 
 import java.util.List;
-import javax.persistence.*;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "salles")
